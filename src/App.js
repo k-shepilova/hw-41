eb161135cbed33779552aaf7790cc7b9dc6a8887
Kateryna Shepilova <kateryna.shepilova@md.p2h.com>
2024-04-28 18:19:27 +0300

    hw-41

--- src/App.js
+++ src/App.js
@@ -1,24 +1,74 @@
-import logo from './logo.svg';
+import { useState } from 'react';
+import { Formik, Form, Field, ErrorMessage } from 'formik';
+
 import './App.css';
 
 function App() {
+    const [result, setResult] = useState(null);
+  const validate = values => {
+    const errors = {};
+    if (!values.name) {
+      errors.name = 'Name is required';
+    }
+    if (!values.email) {
+      errors.email = 'Email is required';
+    } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)) {
+      errors.email = 'Invalid email address';
+    }
+    if (!values.phone) {
+      errors.phone = 'Phone number is required';
+    } else if (!/^[0-9]{10}$/.test(values.phone)) {
+      errors.phone = 'Phone number must be exactly 10 digits';
+    }
+    return errors;
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="App">
+          <h1>Register Form</h1>
+          <Formik
+              initialValues={{name: '', email: '', phone: ''}}
+              validate={validate}
+              onSubmit={(values, {setSubmitting}) => {
+                  setResult(values);
+                  setSubmitting(false);
+              }}
+          >
+              {({isSubmitting}) => (
+                  <Form>
+                      <div className="col">
+                          <label htmlFor="name">Name:</label>
+                          <Field type="text" name="name"/>
+                          <ErrorMessage name="name" component="div" className="err"/>
+                      </div>
+
+                      <div className="col">
+                          <label htmlFor="email">Email:</label>
+                          <Field type="email" name="email"/>
+                          <ErrorMessage name="email" component="div" className="err"/>
+                      </div>
+
+                      <div className="col">
+                          <label htmlFor="phone">Phone:</label>
+                          <Field type="text" name="phone"/>
+                          <ErrorMessage name="phone" component="div" className="err"/>
+                      </div>
+
+                      <button type="submit" disabled={isSubmitting}>
+                          Submit
+                      </button>
+                  </Form>
+              )}
+          </Formik>
+          {result && (
+              <div>
+                  <h3>Your registration data:</h3>
+                  <div>Name: {result.name}</div>
+                  <div>Email: {result.email}</div>
+                  <div>Phone: {result.phone}</div>
+              </div>
+          )}
+      </div>
   );
 }
 
